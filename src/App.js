6c3948c8f205303c698a0a51bd1985f0ab7bb033
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2022-05-05 19:11:36 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Grid = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="play-area">
+      <div id="block_0" class="block">X</div>
+      <div id="block_1" class="block">O</div>
+      <div id="block_2" class="block"></div>
+      <div id="block_3" class="block"></div>
+      <div id="block_4" class="block"></div>
+      <div id="block_5" class="block"></div>
+      <div id="block_6" class="block"></div>
+      <div id="block_7" class="block"></div>
+      <div id="block_8" class="block"></div>
     </div>
   );
+};
+
+const App = () => {
+  return (
+    <div class="container">
+      <h1>Tic-Tac-Toe</h1>
+      <Grid />
+    </div>
+  )
 }
 
 export default App;
